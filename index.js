672130096c571a9813bfbb70b0039b35d1eec7e0
Perry Jorss <pjorss123@gmail.com>
2022-04-20 16:54:41 -0400

    commit tree 19e9a1ddf286927d6494896396215c5416db55f5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(records){
+   const result = records.find(element => element.result ==='W')
+    if (result == undefined){
+        return undefined
+    } else{
+        return result.year
+    }
+    return result
+}
